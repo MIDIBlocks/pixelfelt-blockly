c402100e2aa4697fefac2b9cfed016457100338c
Oz <sitemodder@gmail.com>
2021-07-17 18:32:21 -0700

    #0 Updated branding from pixelfelt to sitemodder

--- quasar.conf.js
+++ quasar.conf.js
@@ -79,8 +79,8 @@ module.exports = function (ctx) {
       
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
-      publicPath: process.env.NODE_ENV === 'production' ? `/pixelfelt-blockly@${pkg.version}/dist/` : env.PUBLIC_PATH,
-      vueRouterBase: process.env.NODE_ENV === 'production' ? `/pixelfelt-blockly@${pkg.version}/dist/` : env.APP_BASE,
+      publicPath: process.env.NODE_ENV === 'production' ? `/sitemodder@${pkg.version}/dist/` : env.PUBLIC_PATH,
+      vueRouterBase: process.env.NODE_ENV === 'production' ? `/sitemodder@${pkg.version}/dist/` : env.APP_BASE,
       distDir: env.BUILD_DIR,
       
       // transpile: false,
